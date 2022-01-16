9998db4f63a588c0368ab5da1ba10a3ca6548e56
AhmedMohamed <ahmedmohmed12363@yahoo.com>
2022-01-16 12:04:12 +0200

    2.0.6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function handleMiddleware() {
+module.exports = function handleMiddleware() {
     return async (req, res) => {
         // set ip for express-rate-limit middleware
         if (!req.ip) req.ip = req.headers["x-real-ip"] || req.connection.remoteAddress;
@@ -7,43 +7,46 @@ function handleMiddleware() {
             // get arg in var
             for (const arg of arguments) {
                 if (typeof arg == 'function') middlewares.push(arg);
-                else if (typeof arg == 'object') options = arg;
-            }
-            const { allowedMethods = [] } = options;
-
-            // // 1- Handle allowedMethods option
-            if (Array.isArray(allowedMethods) && allowedMethods.length > 0)
-                // add handleAllowMethods function before last callback to run after cors if exist
-                middlewares.splice(middlewares.length - 1, 0, handleAllowMethods)
-            function handleAllowMethods(req, res, next, stop) {
-                let onlyAllowedMethods = [];
-                for (const method of allowedMethods)
-                    if (typeof method == 'string') 
-                        onlyAllowedMethods.push(method.toLowerCase())
-                
-                if (!onlyAllowedMethods.includes(req.method.toLowerCase())) {
-                    res.status(404).send();
-                    stop();
+                else if (typeof arg == 'object') {
+                    if ('allowedMethods' in arg) 
+                        middlewares.push(handleAllowMethods)
+                    options = arg;
                 }
-                else next();
             }
-
+            function handleAllowMethods(req, res, next) 
+            {
+                const { allowedMethods = [] } = options;
+                if (Array.isArray(allowedMethods) && allowedMethods.length > 0) {
+                    let onlyAllowedMethods = [];
+                    for (const method of allowedMethods)
+                        if (typeof method == 'string') 
+                            onlyAllowedMethods.push(method.toLowerCase())
+                    
+                    if (!onlyAllowedMethods.includes(req.method.toLowerCase())) {
+                        res.status(404).send();
+                    }
+                    else next();
+                } else next();
+            }
             // handle middlewares
             if (middlewares[0] || req.method != "OPTIONS") 
             {
                 function runNextFunction(func) 
                 {
                     return new Promise(async (resolve, reject) => {
-                        let isCallbackRun = false;
-                        await func(req, res, async function (result) {
-                            isCallbackRun = true;
-                            if (result instanceof Error) return reject(result);
-                            else return resolve();
-                        })
-                        if (!isCallbackRun) reject();
+                        try {
+                            await func(req, res, async function (result) {
+                                if (result instanceof Error) return reject(result);
+                                else return resolve();
+                            })
+                        } catch (error) 
+                        {
+                            console.log(error);
+                            return reject(error);
+                        }
+                        finally { res.finished && reject(); }
                     })
                 }
-
                 for (const func of middlewares) {
                     try { await runNextFunction(func); } 
                     catch (e) { break; }
@@ -53,4 +56,3 @@ function handleMiddleware() {
         catch (error) { console.log(error) }
     };    
 }
-module.exports = handleMiddleware;
